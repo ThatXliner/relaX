d92cc47d55a8313165a7fb614f5d0c0d308862a8
theXliner <bryan.hu.cn@gmail.com>
2020-08-10 13:39:36 -0700

    Update time

--- relaX/time.py
+++ relaX/time.py
@@ -18,9 +18,9 @@ import time
 
 
 def getTime() -> str:
-    """
-    Returns the current time. In a easy-to-process,
-    human-readable, format. Kinda raw though...
+    """Returns the current time.
+
+    In a easy-to-process, human-readable, format. Kinda raw though...
     """
     return str(time.strftime(time.asctime(time.localtime(time.time()))))
 
@@ -92,33 +92,29 @@ class Date(object):
         day=time.gmtime(time.time()).tm_mday,
         year=time.gmtime(time.time()).tm_year,
     ) -> None:
-        """This date object is a object wrapper of all of the functions defined in this
-        file.
+        """This date object is a object wrapper of all of the functions defined in this file.
 
-        :param type month: The month to set the date to. Defaults to time.gmtime(time.time()).tm_mon.
-        :param type day: The day to set the date to. Defaults to time.gmtime(time.time()).tm_mday.
-        :param type year: The year to set the date to. Defaults to time.gmtime(time.time()).tm_year.
+        :param type month: The month to set the date to. Defaults to
+        time.gmtime(time.time()).tm_mon.
+        :param type day: The day to set the date to. Defaults to
+        time.gmtime(time.time()).tm_mday.
+        :param type year: The year to set the date to. Defaults to
+        time.gmtime(time.time()).tm_year.
         :return: Nothing.
         :rtype: None
 
         """
-        # NOTE: Should the take-care method be truncation or modulo?
         self.month = abs(int(float(month)))
-        month = self.month  # Optimization.
-        self.month = month if month <= 12 else (month % 12) + 1
+        tmonth = self.month  # Optimization.
+        self.month = tmonth if tmonth <= 12 else (tmonth % 12) + 1
         self.year = abs(int(float(year)))
-        year = self.year  # Optimization.
+        mday = self._calculate_mday(tmonth, self.year)
         self.day = abs(int(float(day)))
-        day = self.day  # Optimization.
-        if year % 4 == 0 and month == 2:  # Leap year AND February.
-            mdays = 29
-            self.day = day if day <= mdays else (day % mdays) + 1
-        elif day > (30 if month % 2 == 0 else 31):
-            mdays = 30 if month % 2 == 0 else 31
-            mdays = 28 if month % 2 == 0 else mdays
-            self.day = day % mdays + 1
+        tday = self.day  # Optimization.
+        self.day = tday if tday <= mday else (tday % mday) + 1
         self.date = [self.month, self.day, self.year]
         self.json_date = dict(zip(["month", "day", "year"], self.date))
+        self.mday = mday
 
     def __str__(self) -> str:
         """__str__ method."""
@@ -142,6 +138,16 @@ class Date(object):
         """
         return self.json_date.get(key.lower())
 
+    def _calculate_mday(self, tmonth, year):
+        if tmonth % 2 == 0:  # Even month
+            if tmonth == 2:
+                mday = 29 if year % 4 == 0 else 28
+            else:
+                mday = 30
+        else:
+            mday = 31
+        return mday
+
     def update_time(self) -> None:
         """To update the time.
 
@@ -157,7 +163,7 @@ class Date(object):
         self.date = [self.month, self.day, self.year]
         self.json_date = dict(zip(["month", "day", "year"], self.date))
 
-    def daysBeforeToday(self, amount: int) -> str:
+    def days_before_today(self, amount: int) -> str:
         """Returns a string representing some number of days before today.
 
         :param int amount: How many days before today do you want to get?
@@ -165,4 +171,34 @@ class Date(object):
         :rtype: str
 
         """
-        pass
+        amount = int(float(amount))
+        return str(
+            Date(month=self.month, day=self.day, year=self.year).add_days(amount * -1)
+        )
+
+    def add_days(self, amount: int) -> None:
+        """Increments the amount of days after today.
+
+        :param int amount: The amount of days to add.
+        :return: Nothing.
+        :rtype: None
+
+        """
+        amount = abs(int(float(amount)))
+        if self.day + amount > self.mday:  # Next month
+            if self.month + 1 > self.year:  # Next year
+                self.year += 1  # Increment year
+                self.day += amount  # Increment day
+                # Increment month
+                self.month += (  # Calculate months
+                    self.day // self._calculate_mday(self.month, self.year)
+                ) % 12  # Turn it into months
+                # Modulo incremented days
+                self.day = self.day % self._calculate_mday(self.month, self.year)
+            else:  # Not next year
+                self.month += 1  # Increment month
+                self.day += amount  # Increment day
+                # Modulo incremented days
+                self.day = self.day % self._calculate_mday(self.month, self.year)
+        else:  # Normal
+            self.day += amount
